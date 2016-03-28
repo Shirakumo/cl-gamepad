5c24504c500e720a9c93f6886949344a05bd130a
Shinmera <shinmera@tymoon.eu>
2016-03-28 20:59:25 +0200

    Added higher level convenience functions that prevent any kind of leakage.

--- wrapper.lisp
+++ wrapper.lisp
@@ -6,11 +6,49 @@
 
 (in-package #:org.shirakumo.fraf.gamepad)
 
-(defun device-attached (device))
-(defun device-removed (device))
-(defun button-pressed (button time device))
-(defun button-released (button time device))
-(defun axis-moved (axis last-value value time device))
+(defmacro define-alias (name original)
+  `(setf (fdefinition ',name) (fdefinition ',original)))
+
+(define-alias id device-id)
+(define-alias vendor device-vendor)
+(define-alias product device-product)
+(define-alias description device-description)
+(define-alias axis-count device-axis-count)
+(define-alias button-count device-button-count)
+
+(defun device-axis (device axis)
+  (mem-aref (device-axis-states device) :float axis))
+
+(defun device-axes (device)
+  (let* ((size (device-axis-count device))
+         (p (device-axis-states device))
+         (array (make-array size)))
+    (dotimes (i size array)
+      (setf (aref array i) (mem-aref p :float i)))))
+
+(defun device-button (device button)
+  (mem-aref (device-button-states device) :bool button))
+
+(defun device-buttons (device)
+  (let* ((size (device-button-count device))
+         (p (device-button-states device))
+         (array (make-array size)))
+    (dotimes (i size array)
+      (setf (aref array i) (mem-aref p :float i)))))
+
+(defun device-plist (device)
+  `(:id ,(device-id device)
+    :description ,(device-description device)
+    :vendor ,(device-vendor device)
+    :product ,(device-product device)
+    :axis-states ,(device-axes device)
+    :button-states ,(device-buttons device)))
+
+(defun device-attached (device) (declare (ignore device)))
+(defun device-removed (device) (declare (ignore device)))
+(defun button-pressed (button time device) (declare (ignore button time device)))
+(defun button-released (button time device) (declare (ignore button time device)))
+(defun axis-moved (axis last-value value time device) (declare (ignore axis last-value value time device)))
 
 (defun init ()
   (gamepad-device-attach-func (callback device-attach-func) (null-pointer))
@@ -27,7 +65,11 @@
   (gamepad-num-devices))
 
 (defun device (index)
-  (gamepad-device-at index))
+  (gamepad-device-at-index index))
+
+(defun devices ()
+  (loop for i from 0 below (device-count)
+        collect (device i)))
 
 (defun detect-devices ()
   (gamepad-detect-devices))

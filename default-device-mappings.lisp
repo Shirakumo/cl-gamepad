58543bc5b93bb1b4ca3c8942b8ebb748b2516958
Shinmera <shinmera@tymoon.eu>
2020-02-16 11:57:18 +0100

    Provide mapping for DS4

--- default-device-mappings.lisp
+++ default-device-mappings.lisp
@@ -11,6 +11,12 @@
 ;;;
 (in-package "ORG.SHIRAKUMO.FRAF.GAMEPAD")
 
+(define-device-mapping (:DINPUT 1356 2508)
+  :name "Wireless Controller"
+  :buttons (1 :A 2 :B 0 :X 3 :Y 4 :L1 6 :L2 10 :L3 5 :R1 7 :R2 11 :R3 8 :SELECT
+            12 :HOME 9 :START)
+  :axes (3 :L2 4 :R2 0 :L-H 1 :L-V 2 :R-H 5 :R-V 8 :DPAD-V))
+
 (define-device-mapping (:DINPUT 1411 8288)
   :name "USB,2-axis 8-button gamepad  "
   :buttons (1 :A 0 :B 3 :X 2 :Y 4 :L1 5 :R1 6 :SELECT 7 :START)

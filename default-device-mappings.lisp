06643306a18ce3207a29bc32f196e31c95bfe18b
Shinmera <shinmera@tymoon.eu>
2020-02-16 14:47:08 +0100

    Provide some more mappings

--- default-device-mappings.lisp
+++ default-device-mappings.lisp
@@ -21,3 +21,44 @@
   :name "Buffalo BSGP801"
   :buttons (1 :A 0 :B 3 :X 2 :Y 4 :L1 5 :R1 6 :SELECT 7 :START)
   :axes (0 :DPAD-H 1 :DPAD-V))
+
+(define-device-mapping (:EVDEV 1356 616)
+  :name "Sony Dual Shock 3"
+  :buttons (304 :A 305 :B 308 :X 307 :Y 312 :L2 310 :L1 311 :R1 313 :R2 317 :L3
+            318 :R3 546 :DPAD-L 547 :DPAD-R 544 :DPAD-U 545 :DPAD-D 314 :SELECT
+            316 :HOME 315 :START)
+  :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V))
+
+(define-device-mapping (:EVDEV 1356 2508)
+  :name "Sony Dual Shock 4"
+  :buttons (304 :A 305 :B 308 :X 307 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
+            318 :R3 314 :SELECT 316 :HOME 315 :START)
+  :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V))
+
+(define-device-mapping (:EVDEV 10462 4354)
+  :name "Steam Controller"
+  :buttons (304 :A 305 :B 307 :X 308 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
+            318 :R3 290 :DPAD-L 289 :SELECT 314 :HOME 316 :HOME 315 :START)
+  :axes (21 :L2 20 :R2 0 :L-H 1 :R-H 4 :R-V 3 :R-H))
+
+(define-device-mapping (:IOKIT 1356 616)
+  :name "Sony Dual Shock 3"
+  :buttons (589839 :A 589838 :B 589840 :X 589837 :Y 589835 :L1 589833 :L2
+            589826 :L3 589836 :R1 589834 :R2 589827 :R3 589832 :DPAD-L 589830
+            :DPAD-R 589829 :DPAD-U 589831 :DPAD-D 589825 :SELECT 589841 :HOME
+            589828 :START)
+  :axes (65584 :L-H 65585 :L-V 65586 :R-H 65589 :R-V))
+
+(define-device-mapping (:IOKIT 1356 2508)
+  :name "Sony Dual Shock 4"
+  :buttons (589826 :A 589827 :B 589825 :X 589828 :Y 589829 :L1 589831 :L2
+            589835 :L3 589830 :R1 589832 :R2 589836 :R3 589833 :SELECT 589837
+            :HOME 589834 :START)
+  :axes (65587 :L2 65588 :R2 65584 :L-H 65585 :L-V 65586 :R-H 65589 :R-V 65650
+         :DPAD-H 65651 :DPAD-V))
+
+(define-device-mapping (:IOKIT 1411 8288)
+  :name "Buffalo BSGP801"
+  :buttons (589826 :A 589825 :B 589828 :X 589827 :Y 589829 :L1 589830 :R1
+            589831 :SELECT 589832 :START)
+  :axes (65584 :DPAD-H 65585 :DPAD-V))

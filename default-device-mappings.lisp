f0bb74a00a53b2bcf00f3caa63591c566c1914e6
Shinmera <shinmera@tymoon.eu>
2020-12-05 11:58:35 +0100

    Redo mappings on Linux to ensure proper orientations.

--- default-device-mappings.lisp
+++ default-device-mappings.lisp
@@ -28,7 +28,7 @@
   :name "Sony DualShock 3"
   :buttons (2 :A 1 :B 3 :X 0 :Y 6 :L1 4 :L2 10 :L3 7 :R1 5 :R2 11 :R3 9 :SELECT
             12 :HOME 8 :START)
-  :axes (3 :L2 4 :R2 0 :L-H 1 :L-V 2 :R-H 5 :R-V 33 :DPAD-V 32 :DPAD-H)
+  :axes (3 :L2 4 :R2 0 :L-H 1 :L-V 2 :R-H 5 :R-V 32 :DPAD-H 33 :DPAD-V)
   :orientations NIL)
 
 (define-device-mapping (:DINPUT 1356 1476)
@@ -62,23 +62,23 @@
   :name "8BitDo SN30 Pro+"
   :buttons (304 :A 305 :B 306 :X 307 :Y 308 :L1 312 :L3 309 :R1 313 :R3 310
             :SELECT 139 :HOME 311 :START)
-  :axes (2 :L2 5 :L-H 0 :L-H 1 :R-H 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
-  :orientations NIL)
+  :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
+  :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 -1.0 5 1.0 16 1.0 17 -1.0))
 
 (define-device-mapping (:EVDEV 1133 49693)
   :name "Logitech Gamepad F310"
   :buttons (304 :A 305 :B 307 :X 308 :Y 310 :L1 317 :L3 311 :R1 318 :R3 314
             :SELECT 316 :HOME 315 :START)
   :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
-  :orientations NIL)
+  :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 -1.0 5 1.0 16 1.0 17 -1.0))
 
 (define-device-mapping (:EVDEV 1356 616)
   :name "Sony DualShock 3"
   :buttons (304 :A 305 :B 308 :X 307 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
-            318 :R3 546 :DPAD-L 547 :DPAD-R 544 :DPAD-U 545 :DPAD-D 314 :SELECT
+            318 :R3 547 :DPAD-L 546 :DPAD-R 544 :DPAD-U 545 :DPAD-D 314 :SELECT
             316 :HOME 315 :START)
-  :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V)
-  :orientations NIL)
+  :axes (2 :L2 5 :L-H 0 :L-H 1 :L-V 3 :R-H 4 :R-V)
+  :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 -1.0 5 -1.0))
 
 (define-device-mapping (:EVDEV 1356 1476)
   :name "Sony DualShock 4"
@@ -92,50 +92,50 @@
   :buttons (304 :A 305 :B 308 :X 307 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
             318 :R3 314 :SELECT 316 :HOME 315 :START)
   :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
-  :orientations NIL)
+  :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 -1.0 5 1.0 16 1.0 17 -1.0))
 
 (define-device-mapping (:EVDEV 1356 3302)
   :name "Sony DualSense"
   :buttons (305 :A 306 :B 304 :X 307 :Y 308 :L1 310 :L2 314 :L3 309 :R1 311 :R2
             315 :R3 312 :SELECT 316 :HOME 313 :START)
   :axes (3 :L2 4 :R2 0 :L-H 1 :L-V 2 :R-H 5 :R-V 16 :DPAD-H 17 :DPAD-V)
-  :orientations (3 1.0 4 1.0 0 1.0 1 -1.0 2 1.0 5 -1.0 16 1.0 17 -1.0))
+  :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 1.0 5 -1.0 16 1.0 17 -1.0))
 
 (define-device-mapping (:EVDEV 1406 8201)
   :name "Pro Controller"
   :buttons (304 :A 305 :B 306 :X 307 :Y 308 :L1 310 :L2 314 :L3 309 :R1 311 :R2
             315 :R3 312 :SELECT 316 :HOME 313 :START)
-  :axes (16 :DPAD-H 17 :DPAD-V 0 :L-H 1 :L-V 3 :R-H 4 :R-V)
-  :orientations NIL)
+  :axes (0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
+  :orientations (0 1.0 1 -1.0 3 1.0 4 -1.0 16 1.0 17 -1.0))
 
 (define-device-mapping (:EVDEV 10462 4354)
   :name "Steam Controller"
   :buttons (304 :A 305 :B 307 :X 308 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
             318 :R3 290 :DPAD-L 289 :SELECT 314 :HOME 316 :HOME 315 :START)
-  :axes (21 :L2 20 :R2 0 :L-H 1 :R-H 4 :R-V 3 :R-H)
+  :axes (21 :L2 20 :R2 0 :L-H 1 :R-H 3 :R-H 4 :R-V)
   :orientations NIL)
 
 (define-device-mapping (:EVDEV 10462 4418)
   :name "Wireless Steam Controller"
   :buttons (304 :A 305 :B 307 :X 308 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
-            318 :R3 290 :DPAD-L 289 :SELECT 546 :DPAD-L 547 :DPAD-R 544 :DPAD-U
-            545 :DPAD-D 314 :SELECT 316 :HOME 315 :START)
-  :axes (21 :L2 20 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
-  :orientations NIL)
+            318 :R3 290 :DPAD-L 547 :DPAD-L 546 :DPAD-R 544 :DPAD-U 545 :DPAD-D
+            289 :SELECT 314 :SELECT 316 :HOME 315 :START)
+  :axes (21 :L2 0 :L-H 20 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
+  :orientations (0 1.0 1 -1.0 3 1.0 4 -1.0 16 1.0 17 -1.0 20 1.0 21 1.0))
 
 (define-device-mapping (:EVDEV 11720 24834)
   :name "8BitDo SN30 Pro+"
   :buttons (305 :A 304 :B 308 :X 307 :Y 310 :L1 312 :L2 317 :L3 311 :R1 313 :R2
             318 :R3 314 :SELECT 306 :HOME 315 :START)
-  :axes (0 :L-H 1 :R-H 2 :R-H 5 :R-V 16 :DPAD-H 17 :DPAD-V)
-  :orientations NIL)
+  :axes (0 :L-H 1 :L-V 2 :R-H 5 :R-V 16 :DPAD-H 17 :DPAD-V)
+  :orientations (0 1.0 1 -1.0 2 1.0 5 -1.0 16 1.0 17 -1.0))
 
 (define-device-mapping (:IOKIT 1133 49674)
   :name "WingMan RumblePad"
-  :buttons (589825 :A 589826 :B 589827 :C 589828 :X 589829 :Y 589830 :Z 589833
-            :SELECT 589831 :L1 589832 :R1)
-  :axes (65650 :DPAD-H 65651 :DPAD-V 65584 :L-H 65585 :L-V 65589 :R-H 65590
-         :R-V 65586 :THROTTLE)
+  :buttons (589825 :A 589826 :B 589827 :C 589828 :X 589829 :Y 589830 :Z 589831
+            :L1 589832 :R1 589833 :SELECT)
+  :axes (65584 :L-H 65585 :L-V 65589 :R-H 65590 :R-V 65650 :DPAD-H 65651
+         :DPAD-V 65586 :THROTTLE)
   :orientations NIL)
 
 (define-device-mapping (:IOKIT 1356 616)
@@ -168,7 +168,7 @@
 (define-device-mapping (:IOKIT 1356 3302)
   :name "Sony DualSense"
   :buttons (589826 :A 589827 :B 589825 :X 589828 :Y 589829 :L1 589831 :L2
-            589830 :R1 589835 :L3 589832 :R2 589836 :R3 589833 :SELECT 589837
+            589835 :L3 589830 :R1 589832 :R2 589836 :R3 589833 :SELECT 589837
             :HOME 589834 :START)
   :axes (65587 :L2 65588 :R2 65584 :L-H 65585 :L-V 65586 :R-H 65589 :R-V 65650
          :DPAD-H 65651 :DPAD-V)

6a18667cda980f3295c97af948078824853b87c9
Shinmera <shinmera@tymoon.eu>
2021-07-30 12:42:44 +0200

    Xbox Elite Series 2 mapping.

--- default-device-mappings.lisp
+++ default-device-mappings.lisp
@@ -79,6 +79,13 @@
   :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
   :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 -1.0 5 1.0 16 1.0 17 -1.0))
 
+(define-device-mapping (:EVDEV 1118 2816)
+  :name "Xbox Elite Series 2"
+  :buttons (304 :A 305 :B 307 :X 308 :Y 310 :L1 317 :L3 311 :R1 318 :R3 314
+            :SELECT 316 :HOME 315 :START)
+  :axes (2 :L2 5 :R2 0 :L-H 1 :L-V 3 :R-H 4 :R-V 16 :DPAD-H 17 :DPAD-V)
+  :orientations (0 1.0 1 -1.0 2 1.0 3 1.0 4 -1.0 5 1.0 16 1.0 17 -1.0))
+
 (define-device-mapping (:EVDEV 1133 49693)
   :name "Logitech Gamepad F310"
   :buttons (304 :A 305 :B 307 :X 308 :Y 310 :L1 317 :L3 311 :R1 318 :R3 314
